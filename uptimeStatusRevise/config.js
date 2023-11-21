a6807ae58f84bb3e4e91a1f573e413518f4b1d35
GuGuan123 <guguanming@outlook.com>
2023-11-22 00:20:21 +0800

    更新UptimeRobot api密钥

--- uptimeStatusRevise/config.js
+++ uptimeStatusRevise/config.js
@@ -10,15 +10,14 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm794595568-24e01d0a152155ac1a0617fa',
     'm794596843-295afe311ab194a23c70aa61',
     'm794595585-1d0d7aaf0abc05ff145f3603',
     'm794600524-8c03625a22d10d5efe703cb7',
     'm794595596-620ae52f8f9eb6a95efb2c6d',
     'm794607516-59103f34297a15e69ae6dece',
     'm794612021-d7a7a64d5dacc03e83a5abef',
-    'm794936575-0887d0befea43ff8ec1d7f61',
-    'm795402718-6e347f045cd3c9090609b502',
+    'm795402727-4e855e5a09af0d512f4e2697',
+    'm795685589-b26be6c9082f4f68fcb582d1'
   ],
 
   // 是否显示监测站点的链接
@@ -33,11 +32,7 @@ window.Config = {
   Navi: [
     {
       text: '首页',
-      url: 'https://www.i-i.me/507.html'
-    },
-    {
-      text: '关于此页面',
-      url: 'https://github.com/KJZH001/uptimeStatusRevise'
+      url: './'
     }
   ]
 };
