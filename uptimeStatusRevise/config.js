d9de92dfc03fcee4f177a1d0582bb9cc1554f916
GuGuan123 <guguanming@outlook.com>
2024-05-14 00:12:33 +0800

    更换状态页列表

--- uptimeStatusRevise/config.js
+++ uptimeStatusRevise/config.js
@@ -10,14 +10,12 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm794596843-295afe311ab194a23c70aa61',
-    'm794600524-8c03625a22d10d5efe703cb7',
-    'm794595596-620ae52f8f9eb6a95efb2c6d',
     'm794607516-59103f34297a15e69ae6dece',
     'm794612021-d7a7a64d5dacc03e83a5abef',
     'm795402727-4e855e5a09af0d512f4e2697',
     'm795685589-b26be6c9082f4f68fcb582d1',
-    'm796861405-6358a19f36471842c0930f59'
+    'm796861405-6358a19f36471842c0930f59',
+    'm796908583-c76f68887528c0ce685954c8'
   ],
 
   // 是否显示监测站点的链接
